3ec2af99de49e36aabe444d5c2b00a8771140ebe
Amir Kasumovic <amir@ideaology.io>
2021-06-11 15:00:20 +0200

    Timeline posts route

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../Models/Post');
+const User = require('../Models/User');
 
 
 //Create a post
@@ -67,8 +68,22 @@ router.get('/:id', async(req, res) => {
     }catch(error){
         res.status(500).json(error);
     }
-})
+});
 
 //Get timeline posts
+router.get('/timeline/all', async (req, res) => {
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.following.map((friendId) => {
+                return Post.find({userId: friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    }catch(error){
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
