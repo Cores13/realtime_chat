f451ff39c43e70cfdc9c96d307f3b51368c28356
Amir Kasumovic <amir@ideaology.io>
2021-06-11 13:32:01 +0200

    Unfollow a user route

--- routes/users.js
+++ routes/users.js
@@ -53,15 +53,30 @@ router.put('/:id/follow', async (req, res) => {
         const currentUser = await User.findById(req.body.userId);
         if(!user.followers.includes(req.body.userId)){
             await user.updateOne({$push:{followers: req.body.userId}});
-            await currentUser.updateOne({$push:{following: req.body.userId}});
+            await currentUser.updateOne({$push:{following: req.params.id}});
             res.status(200).json('User has been followed');
         }else{
             res.status(403).json('You already follow this user');
         }
     }else{
-        res.status(403).json('You cannot follow yourself');
+        res.status(403).json('You can\'t follow yourself');
     }
 })
 //Unfollow a user
+router.put('/:id/unfollow', async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+        if(user.followers.includes(req.body.userId)){
+            await user.updateOne({$pull:{followers: req.body.userId}});
+            await currentUser.updateOne({$pull:{following: req.params.id}});
+            res.status(200).json('User has been unfollowed');
+        }else{
+            res.status(403).json('You already don\'t follow this user');
+        }
+    }else{
+        res.status(403).json('You cannot unfollow yourself');
+    }
+})
 
 module.exports = router;
