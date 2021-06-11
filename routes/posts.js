443efb06773c1e34cfe7a19a652f9f774a1311a9
Amir Kasumovic <amir@ideaology.io>
2021-06-11 14:07:09 +0200

    Delete post route

--- routes/posts.js
+++ routes/posts.js
@@ -11,12 +11,38 @@ router.post('/', async (req, res) =>{
     }catch(error){
         res.status(500).json(error);
     }
-})
+});
 
 //Update a posts
-
+router.put('/:id', async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body});
+            res.status(200).json('The post has been updated');
+        }else{
+            res.status(403).json('You can update only your posts');
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+    
+});
 //Delete a post
-
+router.delete('/:id', async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.deleteOne({$set: req.body});
+            res.status(200).json('The post has been deleted');
+        }else{
+            res.status(403).json('You can delete only your posts');
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+    
+});
 //Like a post
 
 //Get a post
