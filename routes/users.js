9b5f720b421bf546e6ef94ffafb3281c4b3f7d19
Amir Kasumovic <amir@ideaology.io>
2021-06-11 13:23:43 +0200

    Follow a user route

--- routes/users.js
+++ routes/users.js
@@ -37,9 +37,31 @@ router.delete('/:id', async (req, res) => {
     }
 });
 //Get a user
-
+router.get('/:id', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc;
+        res.status(200).json(other);
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
 //Follow a user
-
+router.put('/:id/follow', async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+        if(!user.followers.includes(req.body.userId)){
+            await user.updateOne({$push:{followers: req.body.userId}});
+            await currentUser.updateOne({$push:{following: req.body.userId}});
+            res.status(200).json('User has been followed');
+        }else{
+            res.status(403).json('You already follow this user');
+        }
+    }else{
+        res.status(403).json('You cannot follow yourself');
+    }
+})
 //Unfollow a user
 
 module.exports = router;
