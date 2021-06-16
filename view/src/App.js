2456db726084b59bc365994da1fa6453723c3823
Amir Kasumovic <amir@ideaology.io>
2021-06-16 14:31:20 +0200

    Register frontend init

--- view/src/App.js
+++ view/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Home from './pages/home/Home.jsx';
 import Profile from './pages/profile/Profile.jsx';
 import Login from './pages/login/Login.jsx';
+import Register from './pages/register/Register.jsx';
 
 function App() {
-  return <Login />
+  return <Register />
 }
 
 export default App;
