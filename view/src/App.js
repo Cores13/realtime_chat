7ada4494792dcb14bf37aeacf146572804b4fe3f
Amir Kasumovic <amir@ideaology.io>
2021-06-16 13:11:58 +0200

    Login frontend init

--- view/src/App.js
+++ view/src/App.js
@@ -2,9 +2,10 @@
 import './App.css';
 import Home from './pages/home/Home.jsx';
 import Profile from './pages/profile/Profile.jsx';
+import Login from './pages/login/Login.jsx';
 
 function App() {
-  return <Profile />
+  return <Login />
 }
 
 export default App;
