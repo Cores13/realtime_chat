d75fbd4b0af1b5e7d2ca104faf45a21c9b62cd94
Amir Kasumovic <amir@ideaology.io>
2021-06-16 14:59:21 +0200

    react-router-dom init

--- view/src/App.js
+++ view/src/App.js
@@ -4,9 +4,32 @@ import Home from './pages/home/Home.jsx';
 import Profile from './pages/profile/Profile.jsx';
 import Login from './pages/login/Login.jsx';
 import Register from './pages/register/Register.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  return <Profile />
+  return (
+    <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+        </Switch>
+    </Router>
+  );
 }
 
 export default App;
