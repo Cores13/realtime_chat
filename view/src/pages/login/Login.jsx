cd97860e3e21a1c9832b475ebf36629d1ea1c26b
Amir Kasumovic <amir@ideaology.io>
2021-06-25 15:58:02 +0200

    context api

--- view/src/pages/login/Login.jsx
+++ view/src/pages/login/Login.jsx
@@ -1,6 +1,20 @@
 import './login.css';
+import {useContext, useRef} from 'react';
+import {loginCall} from '../../apiCalls';
+import {AuthContext} from '../../context/AuthContext';
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+
+    const {user, isFetching, error, dispatch} = useContext(AuthContext);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall({email: email.current.value, password: password.current.value}, dispatch);
+    };
+
+    // console.log(user);
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,13 +23,13 @@ export default function Login() {
                     <span className="loginDesc">Don't feel lonely. You can always meet a stranger here!</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input type="email" placeholder="Email" className="loginInput" ref={email} required/>
+                        <input type="password" placeholder="Password" className="loginInput" ref={password} required minLength="8"/>
                         <button className="loginButton">Log In</button>
                         <span className="loginForgot">Forgot Password?</span>
                         <button className="loginRegisterButton">Create a New Account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
