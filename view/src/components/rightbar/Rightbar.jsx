974379875ea1aa943ead8f77c3edbab69df63124
Amir Kasumovic <amir@ideaology.io>
2021-06-25 14:49:42 +0200

    Feed and profile dynamic data

--- view/src/components/rightbar/Rightbar.jsx
+++ view/src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import './rightbar.css';
 import {Users} from '../../dummyData';
 import Online from '../online/Online';
 
-export default function Rightbar({profile}) {
+export default function Rightbar({user}) {
     const HomeRightbar = () => {
         return (<>
             <h4 className="rightbarTitle">Online Friends</h4>
@@ -20,15 +20,15 @@ export default function Rightbar({profile}) {
             <div className="rightbarInfo">
                 <div className="rightbarInfoItem">
                     <span className="rightbarInfoKey">City: </span>
-                    <span className="rightbarInfoValue">Šićki</span>
+                    <span className="rightbarInfoValue">{user.city}</span>
                 </div>
                 <div className="rightbarInfoItem">
                     <span className="rightbarInfoKey">From: </span>
-                    <span className="rightbarInfoValue">Šićki</span>
+                    <span className="rightbarInfoValue">{user.from}</span>
                 </div>
                 <div className="rightbarInfoItem">
                     <span className="rightbarInfoKey">Relationship: </span>
-                    <span className="rightbarInfoValue">Complicated</span>
+                    <span className="rightbarInfoValue">{user.relationship ===1 ? 'Single' : user.relationship ===2 ? 'Taken' : user.relationship ===3 ? 'Married' : 'Complicated'}</span>
                 </div>
             </div>
             <hr className="rightbarHr" />
@@ -62,7 +62,7 @@ export default function Rightbar({profile}) {
     return (
         <div className="rightbar">
             <div className="rightbarWrapper">
-                {profile ? <ProfileRightbar /> : <HomeRightbar />}
+                {user ? <ProfileRightbar /> : <HomeRightbar />}
             </div>
         </div>
     )
--- view/src/pages/profile/Profile.jsx
+++ view/src/pages/profile/Profile.jsx
@@ -3,19 +3,23 @@ import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Rightbar from '../../components/rightbar/Rightbar';
 import Feed from '../../components/feed/Feed';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router';
 
 
 export default function Profile() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     const [user, setUser] = useState({});
+    const username = useParams().username;
 
     useEffect(() => {
         const fetchUser = async () =>{
-            const res = await axios.get(`/users/${post.userId}`);
+            const res = await axios.get(`/users?username=${username}`);
             setUser(res.data);
         };
         fetchUser();
-    }, [post.userId]);
+    }, [username]);
 
     return (
         <>
@@ -26,18 +30,18 @@ export default function Profile() {
                 <div className="profileRightTop">
                     <div className="profileCover">
                         {/* eslint-disable-next-line */}
-                        <img src={`${PF}post/3.jpeg` || PF+ "person/noCover.jpg"} alt="Cover picture" className="profileCoverImg" />
+                        <img src={user.coverPicture || PF+ "person/noCover.jpg"} alt="Cover picture" className="profileCoverImg" />
                         {/* eslint-disable-next-line */}
-                        <img src={`${PF}person/7.jpeg` || PF+ "person/noAvatar.png"} alt="Profile picture" className="profileUserImg" />
+                        <img src={user.profilePicture || PF+ "person/noAvatar.png"} alt="Profile picture" className="profileUserImg" />
                     </div>
                     <div className="profileInfo">
-                        <h4 className="profileInfoName">Dzix Dzinic</h4>
-                        <span className="profileInfoDesc">Eto Dzixaaaa!</span>
+                        <h4 className="profileInfoName">{user.username}</h4>
+                        <span className="profileInfoDesc">{user.desc}</span>
                     </div>
                 </div>
                 <div className="profileRightBottom">
-                    <Feed username="adi"/>
-                    <Rightbar profile/>
+                    <Feed username={username}/>
+                    <Rightbar user={user}/>
                 </div>
             </div>
         </div>
